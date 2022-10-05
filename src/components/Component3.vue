760d4406a74c4cb8c39790279164e6b0a7d46af9
Temitope Samson Isaiah <61621188+smit1298@users.noreply.github.com>
2022-10-05 06:58:23 +0100

    completed counter app using vuex for state management

---
+++ src/components/Component3.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Component4 />
+    <History />
+  </div>
+</template>
+
+<script>
+import Component4 from "./Component4.vue";
+import History from "./History.vue"
+
+export default {
+  components: {
+    Component4,
+    History,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Component4.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from "./Counter.vue";
+export default {
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<style></style>
